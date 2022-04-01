926ebd2b8276b6f089694f040b96520ab282037f
Matteo Migliarini <33762284+Mamiglia@users.noreply.github.com>
2022-04-01 10:39:17 +0000

    Initial vue template

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # mamiglia.github.io
 My personal Website, and the first website i ever created  
-  
-TO DO: add responsive design
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Sidemenu from './components/Sidemenu.vue';
+</script>
+
+<template>
+  <Sidemenu/>
+  
+</template>
+
+<style>
+@import './assets/base.css';
+Sidemenu {
+  width: 33vw;
+
+}
+</style>
