1eb1200b55a1e239b4d3a79bac5cc7fe8bd371f5
Matteo Migliarini <33762284+Mamiglia@users.noreply.github.com>
2022-04-01 20:31:26 +0000

    Added skills data

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,25 @@
 import Sidemenu from './components/Sidemenu.vue';
 import VueHorizontal from "vue-horizontal";
 import Project from "./components/Project.vue";
+import Skill from "./components/Skill.vue";
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
+}
 
 export default {
   name:"App",
   components: {
     Sidemenu,
     VueHorizontal,
-    Project
+    Project,
+    Skill
   },
   data() {
     return {
@@ -33,7 +45,58 @@ export default {
           text: "This same site is one of my projects. Built in Vue js.",
           gitLink: "https://github.com/Mamiglia/mamiglia.github.io"
         }
-      ]
+      ],
+      skills: shuffleArray([
+        {
+          title: "JavaScript",
+          img: "../../data/images/js.svg"
+        },
+        {
+          title: "Java",
+          img: "../../data/images/java.svg"
+        },
+        {
+          title: "C",
+          img: "../../data/images/c.svg"
+        },
+        {
+          title: "Java Spring",
+          img: "../../data/images/javaspring.svg"
+        },
+        {
+          title: "Kotlin",
+          img: "../../data/images/kotlin.svg"
+        },
+        {
+          title: "PostgreSQL",
+          img: "../../data/images/postgres.svg"
+        },
+        {
+          title: "CSS",
+          img: "../../data/images/css.svg"
+        },
+        {
+          title: "HTML",
+          img: "../../data/images/html.svg"
+        },
+        {
+          title: "Vue.js",
+          img: "../../data/images/vue.svg"
+        },
+        {
+          title: "Python",
+          img: "../../data/images/python.svg"
+        },
+        {
+          title: "Bash",
+          img: "../../data/images/bash.svg"
+        },
+        {
+          title: "prolog",
+          img: "../../data/images/prolog.svg"
+        }
+        
+      ])
     }
   }
   
@@ -55,12 +118,10 @@ export default {
           :gitLink="p.gitLink"/>
       </vue-horizontal>
       <vue-horizontal reactive id="knowledge" class="horizontal">
-          <span v-for="i in 10">
-            <h3>{{i}}</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            </p>
-          </span>
+          <Skill v-for="s in skills" 
+            :title="s.title"
+            :img="s.img"
+            />
       </vue-horizontal>
       <div id="contacts">
   
@@ -80,6 +141,9 @@ export default {
 
 .horizontal {
   width: 90%;
+  padding-top: 10%;
+  height: fit-content ;
+  max-height: 100vw;
 }
 
 #projects {
