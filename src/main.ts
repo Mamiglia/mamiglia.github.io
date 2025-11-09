dec398c73d0eabed40bed829e3dbb10bec1a1962
Mamiglia <33762284+Mamiglia@users.noreply.github.com>
2025-11-09 19:42:57 +0100

    Vue-Toastifier

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// import { CursorFx } from '@luxdamore/vue-cursor-fx'
-// import '@luxdamore/vue-cursor-fx/dist/CursorFx.css'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+import './assets/toast-custom.css'
+
 
 const app = createApp(App)
-// app.component('cursor-fx', CursorFx)
 app.use(router)
+app.use(Toast, {
+  position: 'top-center',
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: 'button',
+  icon: true,
+  rtl: false
+})
 
 app.mount('#app')
