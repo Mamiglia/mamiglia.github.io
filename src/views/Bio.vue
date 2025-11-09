39be4ed68f53599341daf8e4ab22c48400c6fbf7
Mamiglia <33762284+Mamiglia@users.noreply.github.com>
2025-11-09 12:03:40 +0100

    style: update bio and projects descriptions for clarity and detail; adjust typewriter effect delay in Bio.vue for improved user experience

--- src/views/Bio.vue
+++ src/views/Bio.vue
@@ -40,7 +40,7 @@ defineComponent({
           v-if="bioText.length"
           :strings="bioText"
           :autoStart="true"
-          :delay="15"
+          :delay="4"
           :deleteSpeed="100000000000000000000"
           :cursor="'█'"
         />
